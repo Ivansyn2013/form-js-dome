e30075fe4e5e9b11cdc143dab68d68616e1dadaf
titmouse <ivansyn2013@yandex.ru>
2025-04-28 11:49:13 +0300

    Change style to npm and es modules using js

--- src/main.js
+++ src/main.js
@@ -1,24 +1,40 @@
-import { Form } from '@bpmn-io/form-js'
+import { Form } from '@bpmn-io/form-js';
 
-// Загрузка схемы формы
-fetch('./form-build.json')
-  .then(response => response.json())
-  .then(schema => {
-    new Form({
-      container: document.getElementById('form-container'),
-    //   schema: schema
-    schema: {
-        "type": "default",
-        "components": [
-          { "type": "text", "key": "error_field", "label": "Тестовое поле" }
-        ]
-      }
-    })
-    
-  })
-  .catch(error => {  // Перенесённый catch
-    console.error('Ошибка загрузки JSON:', error);
-    fetch('./form-build.json')
-      .then(res => res.text())
-      .then(text => console.log('Сервер вернул:', text));
+const container = document.getElementById('form-container');
+if (!container) console.error('Контейнер не найден!');
+
+// Максимально простая схема
+const schema = {
+  "type": "default",
+  "components": [
+    {
+      "type": "text",
+      "key": "test_field",
+      "label": "Тестовое поле",
+      "description": "Это должно отобразиться"
+    }
+  ]
+};
+const data = {
+  test_field: 'John Doe Company',
+};
+
+// Создаем форму
+const form = new Form({ container, schema });
+form.on('submit', (event) => {
+    console.log(event.data, event.errors);
   });
+
+try {
+    await form.importSchema(schema, data);
+  } catch (err) {
+    console.error('failed to import form', err);
+ };
+
+// Принудительная проверка
+// console.log('Form instance:', form);
+// console.log('Container content:', container.innerHTML);
+console.log('Styles loaded:', 
+    Array.from(document.styleSheets).some(s => 
+      s.href?.includes('form-js.css')
+  ));
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
 import { defineConfig } from 'vite'
 import path from 'path'
+import inspect from 'vite-plugin-inspect';
+
 
 export default defineConfig({
   base: '/form-js-dome/', // Имя репозитория на GitHub
   root: path.resolve(__dirname, 'src'),
+  // resolve: {
+  //   alias: {
+  //     '@assets': path.resolve(__dirname, 'dist/assets'), // Алиас для папки assets
+  //   },
+  // },
+  server: {
+    fs: {
+      // Разрешить доступ к файлам в node_modules
+      allow: ['..'],
+    },
+  },
   build: {
     outDir: '../dist',
-    emptyOutDir: true
-  }
+    emptyOutDir: true,
+    target: 'esnext',
+  },
+  plugins: [inspect()],
+  
 })
